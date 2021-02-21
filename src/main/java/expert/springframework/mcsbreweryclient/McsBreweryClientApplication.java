ae74b792c913099df7fc42c37b0940d6924826c2
jchaconv <jcaaron.chacon@outlook.com>
2021-02-20 23:48:30 -0500

    Initial commit of the project

---
+++ src/main/java/expert/springframework/mcsbreweryclient/McsBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package expert.springframework.mcsbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsBreweryClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/expert/springframework/mcsbreweryclient/McsBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package expert.springframework.mcsbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
